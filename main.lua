2f81693ecaee4814efe787ba8a3aa72c204c32dd
the-overdriven <100090726+the-overdriven@users.noreply.github.com>
2024-12-19 21:51:11 +0100

    working version, finally

--- main.lua
+++ main.lua
@@ -1,81 +1,51 @@
-local function highlight(ref)
-  tes3.createVisualEffect({ magicEffectId = tes3.effect.light, reference = ref })
+local function traverse(nodes, shouldHighlight)
+  if not nodes then
+    return
+  end
 
-  local sceneNode = ref.object.sceneNode
-  for node in table.traverse(sceneNode.children) do
-      if node:isInstanceOfType(ni.type.NiTriShape) then
-          if node.name == 'Editable Mesh' then
-            -- book nodes are weird
-            return
-          end
+  for node in table.traverse(nodes) do
+    if node.materialProperty then
+        local materialProperty = node:detachProperty(0x2):clone()
 
-          -- node doesn't always have material property!
-          if node.materialProperty then
-            node.materialProperty.emissive = { r = 0.5, g = 0.5, b = 0.5 }
-          end
-      end
+        if shouldHighlight then
+          materialProperty.emissive = { r = 0.5, g = 0.5, b = 0.5 }
+        else
+          materialProperty.emissive = { r = 0, g = 0, b = 0 }
+        end
+
+        node:attachProperty(materialProperty)
+    end
   end
 end
 
-local function unhighlight(ref, current)
-  if (ref and current) and (ref.object.id == current.object.id) then
-    return
-  end
+local function highlight(ref)
+  local sceneNode = ref.sceneNode
+  traverse(sceneNode.children, true)
+  sceneNode:updateProperties()
+end
 
-  tes3.removeVisualEffect({ reference = ref })
-
-  if ref.name == 'Editable Mesh' then
-    -- book nodes are weird
-    return
-  end
-
-  local sceneNode = ref.object.sceneNode
-  for node in table.traverse(sceneNode.children) do
-      if node:isInstanceOfType(ni.type.NiTriShape) then
-          if node.materialProperty then
-            local sameNodeInPrevious = nil
-            local isSameNodeInPrevious = nil
-            if current then
-              sameNodeInPrevious = current.object.sceneNode:getObjectByName(node.name)
-              if sameNodeInPrevious then
-                -- compare only if both are not nil
-                isSameNodeInPrevious = (sameNodeInPrevious or node) and sameNodeInPrevious.parent.name == node.parent.name
-              end
-            end
-            
-          
-            if not isSameNodeInPrevious then
-              -- do not unhighlight if this node exists on current object as well
-              node.materialProperty.emissive = { r = 0, g = 0, b = 0 }
-            end                 
-          end
-      end
-  end
+local function unhighlight(ref)
+  local sceneNode = ref.sceneNode
+  traverse(sceneNode.children, false)
+  sceneNode:updateProperties()
 end
 
 local function activateCallback(e)
-  if e.target and (e.target.object.objectType == tes3.objectType.npc) then
-    return
-  end
   unhighlight(e.target)
 end
 event.register(tes3.event.activate, activateCallback)
 
 local function onActivationTargetChanged(e)
-  if e.current and (e.current.object.objectType == tes3.objectType.npc) then
-    return
-  end
-
   if e.current then
-      highlight(e.current)
-   end
-   if e.previous then
-      unhighlight(e.previous, e.current)
-   end
+    highlight(e.current)
+  end
+  if e.previous then
+    unhighlight(e.previous)
+  end
 end
-event.register("activationTargetChanged", onActivationTargetChanged)
+event.register('activationTargetChanged', onActivationTargetChanged)
 
 local function OnInitialized()
-  mwse.log("[Highlighter] lua script loaded")
+  mwse.log('[Highlighter] lua script loaded')
 end
-event.register("initialized", OnInitialized)
+event.register('initialized', OnInitialized)
